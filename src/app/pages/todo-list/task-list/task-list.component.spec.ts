d5b88bf7f064636f5db68b61d3bd72fa1f7615ab
Chris MITEL <mitel.chris@gmail.com>
2019-05-05 01:30:22 +0200

    Create TaskListComponent uni test

--- src/app/pages/todo-list/task-list/task-list.component.spec.ts
+++ src/app/pages/todo-list/task-list/task-list.component.spec.ts
@@ -2,8 +2,9 @@ import { of } from 'rxjs';
 import { TaskStorage } from './../../../services/storage/task-storage.service';
 import { TaskListComponent } from './task-list.component';
 import { CUSTOM_ELEMENTS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { ReverseTabPipe } from 'src/app/pipes/reverse-tab.pipe';
+import { TodoTask } from 'src/app/models/todo-task.model';
 
 @Pipe({name: 'reverseTab'})
 class MockPipe implements PipeTransform {
@@ -24,6 +25,7 @@ describe('TaskListComponent', () => {
     const spy = jasmine.createSpyObj('TaskStorage', ['getTasks', 'addTask', 'deleteTask']);
     spy.addTask.and.returnValue(of({}));
     spy.getTasks.and.returnValue(of([]));
+    spy.deleteTask.and.returnValue(of(true));
 
     pipe = new MockPipe();
 
@@ -49,4 +51,58 @@ describe('TaskListComponent', () => {
   it('should exist', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have onDeleteTask to handle task deletion event', () => {
+    expect(component.onDeleteTask).toBeDefined();
+  });
+
+  it('should have onCheckTask to handle task checking event', () => {
+    expect(component.onCheckTask).toBeDefined();
+  });
+
+  it('should display list of task', () => {
+
+    const hostElement: HTMLElement = fixture.nativeElement;
+
+    expect(hostElement.querySelectorAll('app-todo-task').length).toEqual(0);
+
+    component.taskTab = [ new TodoTask('task1'), new TodoTask('task2') ];
+    fixture.detectChanges();
+
+    expect(hostElement.querySelectorAll('app-todo-task').length).toEqual(2);
+  });
+
+  it('should update the list of task on check task event', () => {
+    expect(true).toBeDefined();
+  });
+
+  it('should update the list of task on delete task event', fakeAsync(() => {
+
+    const hostElement: HTMLElement = fixture.nativeElement;
+
+    const nextSpy = spyOn(component.changeListSubject, 'next').and.callThrough();
+    // const spliceSpy = spyOn(component.taskTab, 'splice').and.callThrough();
+    spyOn(console, 'error').and.callThrough();
+
+    const data = [ new TodoTask('task1'), new TodoTask('task2') ];
+
+    component.taskTab = data;
+
+    fixture.detectChanges();
+
+    expect(component.taskTab.length).toEqual(2);
+
+    component.onDeleteTask(data[0], 0);
+    fixture.detectChanges();
+    tick(10000);
+
+    expect(console.error).not.toHaveBeenCalled();
+    expect(taskStorageSpy.deleteTask).toHaveBeenCalled();
+    expect(nextSpy).toHaveBeenCalled();
+    // expect(spliceSpy).toHaveBeenCalled();
+    expect(component.taskTab.length).toEqual(1);
+    expect(component.taskTab[0].toString()).toEqual('task2');
+    expect(hostElement.querySelectorAll('app-todo-task').length).toEqual(1);
+  }));
+
 });
