f7ef7aa7c0c6f79143b0080b1155f0d0d7592c61
Chris MITEL <mitel.chris@gmail.com>
2019-05-06 18:32:01 +0200

    Sets the default  app page spec

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('new App', () => {
     page = new AppPage();
   });
 
-  it('should be blank', () => {
+  it('should not be blank', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toContain('The world is your oyster.');
+    expect(page.getTitleHeader()).toContain('TodoList');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,12 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.deepCss('app-root ion-content')).getText();
+  getTitleHeader() {
+    return element(by.deepCss('app-root ion-title')).getText();
   }
 }
